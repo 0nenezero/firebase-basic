80c807cdb12e3cce4719e8be8724005b1524cda5
0nenezero <tempo9134@naver.com>
2024-05-07 22:27:05 +0900

    :sparkles: Feat: 로그인 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Router from "./routes/Router";
 import { authService } from "./firebase/fbConfig";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(authService.currentUser);
-  return <Router isLogin={isLogin} />;
+  const [isLogin, setIsLogin] = useState(false);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return init ? <Router isLogin={isLogin} /> : "Loading...";
 }
 
 export default App;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { useState } from "react";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [newAccount, setNewAccount] = useState(true);
+
+  const inputChange = (e) => {
+    const inputData = {
+      type: e.target.type,
+      name: e.target.name,
+      value: e.target.value,
+    };
+
+    if (inputData.name === "email") {
+      setEmail(inputData.value);
+    } else if (inputData.name === "password") {
+      setPassword(inputData.value);
+    }
+  };
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+    let user;
+    try {
+      if (newAccount) {
+        createUserWithEmailAndPassword(auth, email, password).then(
+          (userCredential) => {
+            user = userCredential.user;
+            console.log(user);
+          }
+        );
+      } else {
+        signInWithEmailAndPassword(auth, email, password).then(
+          (userCredential) => {
+            user = userCredential.user;
+            console.log(user);
+          }
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <h2>LoginForm입니다</h2>
+      <form onSubmit={submitLogin}>
+        <label htmlFor="email">
+          이메일:{" "}
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            onChange={inputChange}
+          />
+        </label>
+        <label htmlFor="password">
+          비밀번호:{" "}
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            onChange={inputChange}
+          />
+        </label>
+        <button type="submit">{newAccount ? "회원가입하기" : "로그인"}</button>
+      </form>
+    </>
+  );
 }
 
 export default LoginPage;
